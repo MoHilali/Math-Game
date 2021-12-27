a37d44aeaad2053d9288fcceee3f3a43b962dafe
mohilali <mouad.fdm@gmail.com>
2021-12-27 21:42:13 +0100

    finished game version 1.0. added Q&A, random number generation, activated the answer buttons.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var playing = false;
-var score, timeRemaining, action;
+var score, timeRemaining, action, correctAnswer;;
 
 //if we click on the start/reset
 document.getElementById("startreset").onclick = function() {
@@ -31,14 +31,44 @@ document.getElementById("startreset").onclick = function() {
      }
 }
 
+//clicking on an answer box
+for (var i = 1; i < 5; i++) {
+     document.getElementById("box"+i).onclick = function() {
+          // check if we are playing
+          if (playing) { //yes
+               if (this.innerHTML == correctAnswer) { //correct answer
+                    // increase score by 1
+                    score++;
+
+                    document.getElementById("scoreValue").innerHTML = score;
+                    // hide wrong box and show correct box
+                    hide("wrong");
+                    show("correct");
+                    setTimeout(function() {
+                         hide("correct");
+                    }, 1000);
+
+                    //Generate new Q&A
+                    generateQA();
+
+               } else { //wrong answer
+                    hide("correct");
+                    show("wrong");
+                    setTimeout(function() {
+                         hide("wrong");
+                    }, 1000);
+               }
+          }
+     }
+}
 //functions
 
 //start Counter
 function startCountdown() {
      action = setInterval(function() {
-          timeRemaining --;
+          timeRemaining--;
           document.getElementById("timeremainingvalue").innerHTML = timeRemaining;
-          if(timeRemaining == 0) {//game over
+          if (timeRemaining == 0) { //game over
                stopCountdown();
                show("gameover");
                document.getElementById("gameover").innerHTML = "<p> game over! </p> <p>Your score is: " + score + ".</p>";
@@ -57,26 +87,40 @@ function stopCountdown() {
 }
 
 //hide elements
-function hide(id){
+function hide(id) {
      document.getElementById(id).style.display = "none";
 }
 
 //show elements
-function show(id){
+function show(id) {
      document.getElementById(id).style.display = "block"
 }
 
 //generate question and multiple answers
 
 function generateQA() {
+     var x = 1 + Math.round(Math.random() * 9);
+     var y = 1 + Math.round(Math.random() * 9);
+     correctAnswer = x * y;
 
+     document.getElementById("question").innerHTML = x + "x" + y;
+
+     var correctPosition = 1 + Math.round(Math.random() * 3);
+     document.getElementById("box" + correctPosition).innerHTML = correctAnswer; //fill one box with the correct answer
+
+     //fill other boxes with wrong answers
+     var answers = [correctAnswer];
+     for (var i = 1; i < 5; i++) {
+          if (i != correctPosition) {
+               var wrongAnswer;
+               do {
+                    wrongAnswer = (1 + Math.round(Math.random() * 9)) * (1 + Math.round(Math.random() * 9)); // a wrong answer
+               } while (answers.indexOf(wrongAnswer) > -1);
+               answers.push(wrongAnswer);
+               document.getElementById("box" + i).innerHTML = wrongAnswer;
+          }
+     }
 }
-
-// timeleft?
-//yes -> continue
-//no -> gameover
-//change button to resert
-//generate new Q&A
 
 
 //if we click on answer box
